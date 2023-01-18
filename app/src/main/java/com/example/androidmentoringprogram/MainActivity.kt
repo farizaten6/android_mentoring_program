51e6dacc12f48b1baa5408345d087d3dcbf14e70
Fariza <farizaten8@gmail.com>
2023-01-18 12:54:42 +0600

    FAT-49849: option menu in main activity

--- app/src/main/java/com/example/androidmentoringprogram/MainActivity.kt
+++ app/src/main/java/com/example/androidmentoringprogram/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.androidmentoringprogram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_layout, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.settingsMenuItem -> Toast.makeText(this, "Settings option selected", Toast.LENGTH_SHORT).show()
+            R.id.profileMenuItem -> Toast.makeText(this, "Profile option selected", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
