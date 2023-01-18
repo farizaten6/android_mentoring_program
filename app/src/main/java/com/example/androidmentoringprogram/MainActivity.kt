fe1100ea6bee536bec415e9c87c572dfb0b7f064
Fariza <farizaten8@gmail.com>
2023-01-18 13:32:44 +0600

    FAT-49849: Added drawer layout

--- app/src/main/java/com/example/androidmentoringprogram/MainActivity.kt
+++ app/src/main/java/com/example/androidmentoringprogram/MainActivity.kt
@@ -5,22 +5,43 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toggle = ActionBarDrawerToggle(this, findViewById(R.id.drawerLayout), R.string.open, R.string.close)
+        findViewById<DrawerLayout>(R.id.drawerLayout).addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        findViewById<NavigationView>(R.id.navigationView).setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.settingsMenuItem -> {
+                    Toast.makeText(this, "Settings option selected", Toast.LENGTH_SHORT).show()
+                }
+                R.id.profileMenuItem -> {
+                    Toast.makeText(this, "Profile option selected", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_layout, menu)
+        menuInflater.inflate(R.menu.nav_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            R.id.settingsMenuItem -> Toast.makeText(this, "Settings option selected", Toast.LENGTH_SHORT).show()
-            R.id.profileMenuItem -> Toast.makeText(this, "Profile option selected", Toast.LENGTH_SHORT).show()
+        if (toggle.onOptionsItemSelected(item)){
+            true
         }
         return super.onOptionsItemSelected(item)
     }
