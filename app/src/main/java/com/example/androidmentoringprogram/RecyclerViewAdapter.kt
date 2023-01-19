8fa6b044f341aabeef038baffe49541783ab35f5
Fariza <farizaten8@gmail.com>
2023-01-19 13:54:22 +0600

    FAT-49849: Added recycler view landscape layout

--- app/src/main/java/com/example/androidmentoringprogram/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/androidmentoringprogram/RecyclerViewAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.androidmentoringprogram
 
-import android.content.res.Configuration
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,26 +8,14 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerViewAdapter(
-    private val orientation: Int?
+    private val itemsList: List<Figure>,
+    private val indexShift: Int
 ): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
-    private val figuresList = listOf(
-        Figure(R.drawable.triangle, "Triange"),
-        Figure(R.drawable.circle, "Circle"),
-        Figure(R.drawable.kite, "Kite"),
-        Figure(R.drawable.parallelogram, "Parallelogram"),
-        Figure(R.drawable.pentagon, "Pentagon"),
-        Figure(R.drawable.rectangle, "Rectangle"),
-        Figure(R.drawable.rhombus, "Rhombus"),
-        Figure(R.drawable.square, "Square"),
-        Figure(R.drawable.trapezoid, "Trapezoid"),
-        Figure(R.drawable.hexagon, "Hexagon")
-    )
+
 
     class ViewHolder(item: View): RecyclerView.ViewHolder(item){
         var itemImage: ImageView = item.findViewById(R.id.rcViewItemImage)
         var itemName: TextView = item.findViewById(R.id.rcViewItemText)
-        var itemImage2: ImageView? = item.findViewById(R.id.rcViewItemImage2)
-        var itemName2: TextView? = item.findViewById(R.id.rcViewItemText2)
         var itemNumber: Int? = null
 
         init {
@@ -57,24 +44,12 @@ class RecyclerViewAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-            holder.itemImage.setImageResource(figuresList.get(position).imageId)
-            holder.itemName.text = figuresList.get(position).name
-            holder.itemNumber = position + 1
-        } else {
-            if (position % 2 == 0) {
-                holder.itemImage.setImageResource(figuresList.get(position).imageId)
-                holder.itemName.text = figuresList.get(position).name
-                holder.itemNumber = position + 1
-            } else {
-                holder.itemImage2?.setImageResource(figuresList.get(position).imageId)
-                holder.itemName2?.text = figuresList.get(position).name
-                holder.itemNumber = position + 1
-            }
-        }
+            holder.itemImage.setImageResource(itemsList.get(position).imageId)
+            holder.itemName.text = itemsList.get(position).name
+            holder.itemNumber = position + 1 + indexShift
     }
 
     override fun getItemCount(): Int {
-        return figuresList.size
+        return itemsList.size
     }
 }
--- app/src/main/java/com/example/androidmentoringprogram/RecyclerViewFragment.kt
+++ app/src/main/java/com/example/androidmentoringprogram/RecyclerViewFragment.kt
@@ -1,5 +1,6 @@
 package com.example.androidmentoringprogram
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,19 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerViewFragment : Fragment() {
+
+    private val figuresList = listOf(
+        Figure(R.drawable.triangle, "Triange"),
+        Figure(R.drawable.circle, "Circle"),
+        Figure(R.drawable.kite, "Kite"),
+        Figure(R.drawable.parallelogram, "Parallelogram"),
+        Figure(R.drawable.pentagon, "Pentagon"),
+        Figure(R.drawable.rectangle, "Rectangle"),
+        Figure(R.drawable.rhombus, "Rhombus"),
+        Figure(R.drawable.square, "Square"),
+        Figure(R.drawable.trapezoid, "Trapezoid"),
+        Figure(R.drawable.hexagon, "Hexagon")
+    )
 
     private var layoutManager: RecyclerView.LayoutManager? = null
     private var adapter: RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>? = null
@@ -23,10 +37,24 @@ class RecyclerViewFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val rvView: RecyclerView = view.findViewById(R.id.rcView)
-        rvView.apply {
-            layoutManager = LinearLayoutManager(activity)
-            adapter = RecyclerViewAdapter(activity?.resources?.configuration?.orientation)
+        val rvView2: RecyclerView? = view.findViewById(R.id.rcView2)
+
+        if (activity?.resources?.configuration?.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            rvView.apply {
+                layoutManager = LinearLayoutManager(activity)
+                adapter = RecyclerViewAdapter(figuresList, indexShift = 0)
+            }
+        } else {
+            rvView.apply {
+                layoutManager = LinearLayoutManager(activity)
+                adapter = RecyclerViewAdapter(figuresList.subList(0, 5), indexShift = 0)
+            }
+            rvView2?.apply {
+                layoutManager = LinearLayoutManager(activity)
+                adapter = RecyclerViewAdapter(figuresList.subList(5, 10), indexShift = 5)
+            }
         }
+
         super.onViewCreated(view, savedInstanceState)
     }
 }
